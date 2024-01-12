ebe89aa7163913c19cbdde2ae304f7241fd1b256
FRANCISCO JAVIER FEBLES JIMENEZ <ff2018-3659@uce.edu.do>
2024-01-12 13:38:06 -0400

    seguimientos

--- ProyectoPREP/Controllers/SeguimientosController.cs
+++ ProyectoPREP/Controllers/SeguimientosController.cs
@@ -15,34 +15,24 @@ namespace ProyectoPREP.Controllers
             this.db = _db;
         }
         // GET: SeguimientosController
+        [HttpPost]
         public JsonResult CreatininaMenor60(int IdDatos, Seguimiento seguimiento)
         {
 
             var formulario = db.FormularioPreps.Where(X => X.DatosGeneralesId == IdDatos).FirstOrDefault();
 
             var elegibilidad = db.ElegibilidadPreps.FirstOrDefault(x => x.FormularioPrepId == formulario.Id);
-            var segui = db.Seguimientos.FirstOrDefault(x => x.ElegibilidadPrepId == elegibilidad.Id);
+
+            elegibilidad.Estatus = 6;
+
+            seguimiento.ElegibilidadPrepId = elegibilidad.Id;
+            seguimiento.SeguimimientoPruebaId = 5;
+            seguimiento.Usuario = "1";
 
 
-            using (DbPrepContext db = new DbPrepContext())
-            {
-                segui.FechaRegistroSeguimiento = seguimiento.FechaRegistroSeguimiento;
-                segui.SignosVitalesTa = seguimiento.SignosVitalesTa;
-                segui.SignosVitalesFc = seguimiento.SignosVitalesFc;
-                segui.SignosVitalesFr = seguimiento.SignosVitalesFr;
-                segui.Peso = seguimiento.Peso;
-                segui.TallaPies = seguimiento.TallaPies;
-                segui.TallaPulgadas = seguimiento.TallaPulgadas;
-                segui.FechaPruebaVih = seguimiento.FechaPruebaVih;
-                segui.ResultadoPruebaVih = seguimiento.ResultadoPruebaVih;
-                segui.FechaRegistroSeguimiento = seguimiento.FechaRegistroSeguimiento;
-
-                elegibilidad.Estatus = 6;
-                //db.Entry(elegibilidad).State = EntityState.Modified;
-                //db.Entry(segui).State = EntityState.Modified;
-                //db.SaveChanges();
-
-            }
+            db.Entry(elegibilidad).State = EntityState.Modified;
+            db.Seguimientos.Add(seguimiento);
+            db.SaveChanges();
 
             var result = new
             {
@@ -51,34 +41,26 @@ namespace ProyectoPREP.Controllers
             return Json(result);
         }
 
+        [HttpPost]
         public JsonResult VIHPositivo(int IdDatos, Seguimiento seguimiento)
         {
             bool status = true;
 
             var formu = db.FormularioPreps.FirstOrDefault(X => X.DatosGeneralesId == IdDatos);
             var elegibilidad = db.ElegibilidadPreps.FirstOrDefault(x => x.FormularioPrepId == formu.Id);
-            var segui = db.Seguimientos.FirstOrDefault(x => x.ElegibilidadPrepId == elegibilidad.Id);
 
             elegibilidad.Estatus = 6;
 
-            using (DbPrepContext db = new DbPrepContext())
-            {
-                segui.FechaRegistroSeguimiento = seguimiento.FechaRegistroSeguimiento;
-                segui.SignosVitalesTa = seguimiento.SignosVitalesTa;
-                segui.SignosVitalesFc = seguimiento.SignosVitalesFc;
-                segui.SignosVitalesFr = seguimiento.SignosVitalesFr;
-                segui.Peso = seguimiento.Peso;
-                segui.TallaPies = seguimiento.TallaPies;
-                segui.TallaPulgadas = seguimiento.TallaPulgadas;
-                segui.FechaPruebaVih = seguimiento.FechaPruebaVih;
-                segui.ResultadoPruebaVih = seguimiento.ResultadoPruebaVih;
-                segui.FechaRegistroSeguimiento = seguimiento.FechaRegistroSeguimiento;
+            seguimiento.ElegibilidadPrepId = elegibilidad.Id;
+            seguimiento.SeguimimientoPruebaId = 5;
+            seguimiento.Usuario = "1";
 
-                //db.Entry(elegibilidad).State = EntityState.Modified;
-                //db.Entry(segui).State = EntityState.Modified;
-                //db.SaveChanges();
 
-            }
+            db.Entry(elegibilidad).State = EntityState.Modified;
+            db.Seguimientos.Add(seguimiento);
+            db.SaveChanges();
+
+
 
             var result = new { status };
             return Json(result);
@@ -90,7 +72,6 @@ namespace ProyectoPREP.Controllers
             var datos = db.DatosGenerales.FirstOrDefault(d => d.Id == id);
             var formu = db.FormularioPreps.FirstOrDefault(d => d.DatosGeneralesId == id);
             var elegi = db.ElegibilidadPreps.FirstOrDefault(d => d.FormularioPrepId == formu.Id);
-            var segui = db.Seguimientos.FirstOrDefault(d => d.ElegibilidadPrepId == elegi.Id);
 
 
             ViewBag.Nombre = datos.Nombres + " " + datos.Apellidos;
@@ -100,7 +81,6 @@ namespace ProyectoPREP.Controllers
             ViewBag.Edad = Convert.ToDecimal(datos.Edad);
             ViewBag.Sexo = datos.Sexo;
             ViewBag.Genero = datos.Genero;
-            ViewBag.seguimientosPrep = segui.SeguimimientoPruebaId;
 
 
          
@@ -112,43 +92,20 @@ namespace ProyectoPREP.Controllers
         {
             var formulario = db.FormularioPreps.FirstOrDefault(x => x.DatosGeneralesId == IdDatos);
             var elegibilidad = db.ElegibilidadPreps.FirstOrDefault(x => x.FormularioPrepId == formulario.Id);
-            var segui = db.Seguimientos.FirstOrDefault(x => x.ElegibilidadPrepId == elegibilidad.Id);
 
             seguimiento.ElegibilidadPrepId = elegibilidad.Id;
+            seguimiento.SeguimimientoPruebaId = 5;
             seguimiento.Usuario = "1";
 
+            elegibilidad.Estatus = 6;
 
-            var pruebaId = segui.SeguimimientoPruebaId;
 
-            if (pruebaId == 1)
-            {
-                seguimiento.SeguimimientoPruebaId = 2;
 
-            }
-            if (pruebaId == 2)
-            {
-                seguimiento.SeguimimientoPruebaId = 3;
+            db.Entry(elegibilidad).State = EntityState.Modified;
+            db.Seguimientos.Add(seguimiento);
+            db.SaveChanges();
 
-            }
-            if (pruebaId == 3)
-            {
-                seguimiento.SeguimimientoPruebaId = 4;
-
-            }
-            if (pruebaId == 4)
-            {
-                seguimiento.SeguimimientoPruebaId = 5;
-
-            }
-            if (pruebaId == 5)
-            {
-                seguimiento.SeguimimientoPruebaId = 2;
-
-            }
-            //db.Seguimientos.Add(seguimiento);
-            //db.SaveChanges();
-
-            return View(); 
+            return RedirectToAction("DatosGeneralesPorAprobado", "DatosGenerales");
         }
 
 
